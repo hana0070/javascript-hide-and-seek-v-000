303f7292300b6c87e20c27bf1a670331e6fa8737
github username <github email address>
2017-06-10 14:11:30 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  let deepestChild = document.getElementById("grand-node");
+
+  while ( deepestChild.children.length > 0 ) {
+    deepestChild = deepestChild.children[0]
+  }
+ return deepestChild
+}
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
